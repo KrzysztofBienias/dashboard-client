71558bf087cc857c287afc3b12b52062a646a18c
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2023-03-27 20:02:14 +0200

    :bento: Created theme and typed it

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,20 @@
+import { createTheme } from '@mui/material/styles';
+import { ThemeProvider } from '@mui/material';
+import { useMemo } from 'react';
+import { themeSettings } from './theme';
+import CssBaseline from '@mui/material/CssBaseline';
+
 const App = () => {
-    return <div>App</div>;
+    const theme = useMemo(() => createTheme(themeSettings), []);
+
+    return (
+        <div>
+            <ThemeProvider theme={theme}>
+                <CssBaseline />
+                app
+            </ThemeProvider>
+        </div>
+    );
 };
 
 export default App;
