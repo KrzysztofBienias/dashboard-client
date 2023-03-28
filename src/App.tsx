5ef81f60789a812af2a914c5a1376627e61f5be8
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2023-03-28 20:09:05 +0200

    :sparkles: Added routing

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,27 @@ import { ThemeProvider } from '@mui/material';
 import { useMemo } from 'react';
 import { themeSettings } from './theme';
 import CssBaseline from '@mui/material/CssBaseline';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Navbar from '@/scenes/navbar';
 
 const App = () => {
     const theme = useMemo(() => createTheme(themeSettings), []);
 
     return (
         <div>
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                app
-            </ThemeProvider>
+            <BrowserRouter>
+                <ThemeProvider theme={theme}>
+                    <CssBaseline />
+                    <Box width="100%" height="100%" padding="1rem 2rem 4rem">
+                        <Navbar />
+                        <Routes>
+                            <Route path="/" element={<div>dashboard</div>} />
+                            <Route path="/predictions" element={<div>predictions</div>} />
+                        </Routes>
+                    </Box>
+                </ThemeProvider>
+            </BrowserRouter>
         </div>
     );
 };
