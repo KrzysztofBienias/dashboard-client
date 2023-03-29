43d56dc06cf6b525053cf22ab161a3e08cb66a83
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2023-03-29 22:13:04 +0200

    :sparkles: Created dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import CssBaseline from '@mui/material/CssBaseline';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
 import Navbar from '@/scenes/navbar';
+import Dashboard from '@/scenes/dashboard';
 
 const App = () => {
     const theme = useMemo(() => createTheme(themeSettings), []);
@@ -18,7 +19,7 @@ const App = () => {
                     <Box width="100%" height="100%" padding="1rem 2rem 4rem">
                         <Navbar />
                         <Routes>
-                            <Route path="/" element={<div>dashboard</div>} />
+                            <Route path="/" element={<Dashboard />} />
                             <Route path="/predictions" element={<div>predictions</div>} />
                         </Routes>
                     </Box>
