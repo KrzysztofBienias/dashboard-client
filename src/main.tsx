d7bbb3b0ec321139cfe87e36e038f35ba21eebca
Krzysztof Bienias <krzysztofzbienias@gmail.com>
2023-04-02 19:51:47 +0200

    :bug: Fixed import path of redux toolkit

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from '@/App';
 import '@/index.css';
 import { Provider } from 'react-redux/es/exports';
-import { configureStore } from '@reduxjs/toolkit/dist/configureStore';
+import { configureStore } from '@reduxjs/toolkit';
 import { setupListeners } from '@reduxjs/toolkit/query';
 import { api } from '@/state/api';
 
